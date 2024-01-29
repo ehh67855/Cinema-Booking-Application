f50e47bd22235edbc6539798574b6ac63876b40d
evan hammam <ehh67855@uga.edu>
2024-01-29 16:24:36 -0500

    Initial commit

---
+++ src/test/java/com/csci4050/Cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csci4050.Cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
