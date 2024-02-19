cc49d4615b59cb3780dcb6699c2f0d39530f7997
evan hammam <ehh67855@uga.edu>
2024-02-19 16:01:14 -0500

    Configured React Router Dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,13 @@
-import React, { Component } from 'react';
-import MovieTable from './components/movie/MovieTable'; // Assuming MovieTable component is in the same directory
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
-class App extends Component {
-  state = {
-    movies: []
-  };
-
-  async componentDidMount() {
-    try {
-      const response = await fetch('http://localhost:8080/api/movies');
-      const data = await response.json();
-      this.setState({ movies: data });
-    } catch (error) {
-      console.error('Error fetching movies:', error);
-    }
-  }
-
-  render() {
-    const { movies } = this.state;
-    return (
-      <div className="App">
-        <main className="container">
-          <MovieTable movies={movies} />
-        </main>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
