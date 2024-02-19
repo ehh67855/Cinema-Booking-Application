db3f6d501e50fd15889e869d58ebfab27423f771
evan hammam <ehh67855@uga.edu>
2024-02-19 16:39:46 -0500

    Completed Layout Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import NoPage from './components/NoPage';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Layout><HomePage /> </Layout>} />
+          <Route path="*" element={<Layout><NoPage /></Layout>} />
     </Routes>
   );
 }
