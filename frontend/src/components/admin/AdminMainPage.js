b7fd979c9f9b4cccb2acfb711d2c52802e84e089
rrj03260 <rrj03260@uga.edu>
2024-02-24 17:54:32 -0500

    admin css
    Better looking css for admin pages.

--- frontend/src/components/admin/AdminMainPage.js
+++ frontend/src/components/admin/AdminMainPage.js
@@ -4,9 +4,9 @@ import "./AdminMainPage.css";
 const AdminMainPage = () => {
     return (
         <div className="buttons">
-            <button>Manage Movies</button>
-            <button>Manage Users</button>
-            <button>Manage Promotions</button>
+            <button className="adminMainPageBtn">Manage Movies</button>
+            <button className="adminMainPageBtn">Manage Users</button>
+            <button className="adminMainPageBtn">Manage Promotions</button>
         </div>
     );
 }
